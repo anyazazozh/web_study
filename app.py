1f1949804ff212425899d649d7387e16180e32f0
Анна <kostann@live.ru>
2022-10-22 21:22:35 +0300

    flask left menu question

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ def index():
    surname = 'Kostyuchenko'
    avatar = '/static/assets/avatar (1).png'
 
+   table = [{'c1':'1','c2':'#12594','c3':'Dec 1, 2021','c4':'Frank Murlo','c5':'312 S Wilmette Ave','c6':'$847.69','c7':'New Order','c8':''}]
+
    line_chart_legend = [{'name':'Offline orders', 'dot':'dot_1'},
                         {'name':'Online orders', 'dot':'dot_2'}]
 
@@ -19,12 +21,10 @@ def index():
                           {'name':'Online', 'dot':'dot_2'},
                           {'name':'Trade', 'dot':'dot_1'}]
 
-
-
    cards = [{'total_number':'89,935', 'kpi':'Total users', 'icon':'/static/assets/icon.png', 'bottom_text':'+1.01% this week', 'bottom_number':'10.2', 'bottom_icon':'/static/assets/up.png'},
-            {'total_number':'23,283.5', 'kpi':'Total products', 'icon':'/static/assets/icon.png', 'bottom_text':'+0.49% this week', 'bottom_number':'3.1', 'bottom_icon':'/static/assets/up.png'},
-            {'total_number':'46,827', 'kpi':'Total users', 'icon':'/static/assets/icon.png', 'bottom_text':'-0.91% this week', 'bottom_number':'2.56', 'bottom_icon':'/static/assets/shape (10).png'},
-            {'total_number':'124,854', 'kpi':'Refunded', 'icon':'/static/assets/icon.png', 'bottom_text':'+1.51% this week', 'bottom_number':'7.2', 'bottom_icon':'/static/assets/up.png'}]
+            {'total_number':'23,283.5', 'kpi':'Total products', 'icon':'/static/assets/case (1).png', 'bottom_text':'+0.49% this week', 'bottom_number':'3.1', 'bottom_icon':'/static/assets/up.png'},
+            {'total_number':'46,827', 'kpi':'Total users', 'icon':'/static/assets/check.png', 'bottom_text':'-0.91% this week', 'bottom_number':'2.56', 'bottom_icon':'/static/assets/shape (10).png'},
+            {'total_number':'124,854', 'kpi':'Refunded', 'icon':'/static/assets/arrow-right.png', 'bottom_text':'+1.51% this week', 'bottom_number':'7.2', 'bottom_icon':'/static/assets/up.png'}]
 
    left_bottom_menu = [{'name':'Help Centre', 'icon' : '/static/assets/info.png'},
                         {'name':'Contact us','icon' : '/static/assets/chat.png'},
@@ -37,7 +37,7 @@ def index():
                 {'name':'Checkout','icon':'/static/assets/checkout.png','expand': False, 'number':2},
                 {'name':'Setting','icon':'/static/assets/settings.png','expand': False, 'number':False}]
 
-   return  render_template('index.html', name = name, surname = surname, left_menu = left_menu, left_bottom_menu = left_bottom_menu, avatar = avatar, cards = cards, circle_chart_legend = circle_chart_legend, line_chart_legend = line_chart_legend)
+   return  render_template('index.html', name = name, surname = surname, left_menu = left_menu, left_bottom_menu = left_bottom_menu, avatar = avatar, cards = cards, circle_chart_legend = circle_chart_legend, line_chart_legend = line_chart_legend, table = table)
 
 if __name__ == "__main__":
     app.run(debug = True)
